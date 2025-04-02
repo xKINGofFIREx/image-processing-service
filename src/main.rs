0f180517353a995ee5b5a475d5fb9c58f15e804c
Mavis <artem.ilin.03@mail.ru>
2025-04-02 17:29:08 +0300

    make user info serializable

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use actix_web::{post, HttpResponse, HttpServer, App, Responder};
-use std::io::Result;
+mod user_info;
 
-struct User {
-    username: String,
-    password: String,
-}
+use actix_web::{App, HttpResponse, HttpServer, Responder, post};
+use std::io::Result;
 
 #[post("/login")]
 async fn login(req_body: String) -> impl Responder {
@@ -23,13 +20,8 @@ async fn images(req_body: String) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-   HttpServer::new(|| {
-        App::new()
-            .service(login)
-            .service(images)
-            .service(register)
-    }) 
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(login).service(images).service(register))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
---
+++ src/user_info.rs
@@ -0,0 +1,7 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct User {
+    pub username: String,
+    pub password: String,
+}
